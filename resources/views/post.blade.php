d69a2461edab98a6ecb759f6119b8d97c22d01b6
bangnokia <bangnokia@gmail.com>
2020-11-15 01:47:25 +0700

    update demo page

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,8 +1,8 @@
 <x-layout :title="$post->title">
 
-    <h1 class="text-red-500 text-2xl">{{ $post->title }}</h1>
+    <h1 class="text-red-500 text-4xl">{{ $post->title }}</h1>
 
-    <div class="mt-5 leading-7">
+    <div class="mt-5 leading-7 post-content">
         {!! $post->content !!}
     </div>
 </x-layout>
