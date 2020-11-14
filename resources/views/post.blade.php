b234b873865f7dacb13a6aaa08fa4a45a9c45122
bangnokia <bangnokia@gmail.com>
2020-11-15 01:59:08 +0700

    deploy

--- resources/views/post.blade.php
+++
@@ -1,8 +0,0 @@
-<x-layout :title="$post->title">
-
-    <h1 class="text-red-500 text-4xl">{{ $post->title }}</h1>
-
-    <div class="mt-5 leading-7 post-content">
-        {!! $post->content !!}
-    </div>
-</x-layout>
